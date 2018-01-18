7af8ca6ed9be48b83c10446173d3db059ee52c74
Mike Tsou <mr.tsou@gmail.com>
2018-01-18 12:31:22 -0800

    Implement function that updates todo status by making a put request to the api

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const TodoItem = ({ description, completed, onDelete }) => {
+const TodoItem = ({ description, completed, onDelete, onToggle }) => {
   const styles = {
     textDecoration: completed ? 'line-through' : 'none'
   }
 
   return (
-    <li style={styles}>{description}<span onClick={onDelete}> X </span></li>
+    <li>
+      <span style={styles} onClick={onToggle}>{description}</span>
+      <span onClick={onDelete}> X </span>
+    </li>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -92,12 +92,46 @@ class TodoList extends Component {
     });
   }
 
+  toggleTodo(todo) {
+    const updateURL = `${URL}/${todo._id}`
+
+    fetch(updateURL, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({completed: !todo.completed})
+    })
+    .then(res => {
+      if(!res.ok) {
+        if(res.status >= 400 && res.status < 500) {
+          return res.json().then(data => {
+            let error = { errorMessage: data.message };
+            throw error;
+          })
+        }
+        else {
+          let error = { errorMessage: "Please try again later, server is not responding" };
+          throw error;
+        }
+      }
+      return res.json();
+    })
+    .then(updatedTodo => {
+      const todos = this.state.todos.map(todo =>
+        todo._id === updatedTodo._id ? {...todo, completed: !todo.completed} : todo
+      );
+      this.setState({todos});
+    });
+  }
+
   render() {
     const todos = this.state.todos.map(todo => (
       <TodoItem
         key={todo._id}
         {...todo}
         onDelete={this.deleteTodo.bind(this, todo._id)}
+        onToggle={this.toggleTodo.bind(this, todo)}
       />
     ));
     return (
